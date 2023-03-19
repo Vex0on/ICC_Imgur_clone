a3b56c88bfa20a262f3294ba59d16113c09d283b
MadPapa <happyekkov@gmail.com>
2023-03-19 01:38:42 +0100

    code refactoring in views

--- backend/imgur/views.py
+++ backend/imgur/views.py
@@ -1,122 +1,89 @@
 from rest_framework.decorators import api_view
-from .serializers import *
-from rest_framework.response import Response
-from rest_framework import status
-from django.contrib.auth import authenticate
+from .custom_views.ImgurUser import register_user, login, get_imgur_users, get_imgur_user
+from .custom_views.Post import get_post, get_posts
+from .custom_views.Image import get_image, get_images
+from .custom_views.Comment import get_comment, get_comments
+from .custom_views.Subcomment import get_subcomment, get_subcomments
+from .custom_views.Reaction import get_reaction, get_reactions
 
 
-@api_view(['GET'])
-def get_imgur_users(request):
-    users = ImgurUser.objects.all()
-    serializer = ImgurUserSerializer(users, many=True)
-    return Response(serializer.data)
+# IMGUR USER
+
+@api_view(['POST'])
+def register_user_view(request):
+    return register_user(request)
 
 
 @api_view(['POST'])
-def register_user(request):
-    serializer = ImgurUserSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(
-            {'message': 'User created successfully'},
-            status=status.HTTP_201_CREATED
-        )
-    return Response(
-        status=status.HTTP_400_BAD_REQUEST
-    )
-
-
-@api_view(['POST'])
-def login(request):    
-    username = request.data.get('username')
-    password = request.data.get('password')
-
-    user = authenticate(request, username=username, password=password)
-
-    if user is not None:
-        return Response(
-            {'message': 'HTTP_200_OK'},
-            status=status.HTTP_200_OK
-        )
-    else:
-        return Response(
-            {'message': 'HTTP_404_NOT_FOUND'},
-            status=status.HTTP_404_NOT_FOUND
-        )
+def login_user_view(request):
+    return login(request)
 
 
 @api_view(['GET'])
-def get_imgur_user(request, pk):
-    user = ImgurUser.objects.get(id=pk)
-    serializer = ImgurUserSerializer(user, many=False)
-    return Response(serializer.data)
+def get_users_view(request):
+    return get_imgur_users(request)
 
 
 @api_view(['GET'])
-def get_posts(request):
-    posts = Post.objects.all()
-    serializer = PostSerializer(posts, many=True)
-    return Response(serializer.data)
+def get_user_view(request):
+    return get_imgur_user(request)
+
+
+# POST
+
+@api_view(['GET'])
+def get_post_view(request):
+    return get_post(request)
 
 
 @api_view(['GET'])
-def get_post(request, pk):
-    post = Post.objects.get(id=pk)
-    serializer = PostSerializer(post, many=False)
-    return Response(serializer.data)
+def get_posts_view(request):
+    return get_posts(request)
+
+# Image
 
 
 @api_view(['GET'])
-def get_images(request):
-    images = Image.objects.all()
-    serializer = ImageSerializer(images, many=True)
-    return Response(serializer.data)
+def get_images_view(request):
+    return get_images(request)
 
 
 @api_view(['GET'])
-def get_image(request, pk):
-    image = Image.objects.get(id=pk)
-    serializer = ImageSerializer(image, many=False)
-    return Response(serializer.data)
+def get_image_view(request):
+    return get_image(request)
+
+
+# COMMENT
+
+@api_view(['GET'])
+def get_comment_view(request):
+    return get_comment(request)
 
 
 @api_view(['GET'])
-def get_comments(request):
-    comments = Comment.objects.all()
-    serializer = CommentSerializer(comments, many=True)
-    return Response(serializer.data)
+def get_comments_view(request):
+    return get_comments(request)
+
+
+# SUBCOMMENT
+
+@api_view(['GET'])
+def get_subcomment_view(request):
+    return get_subcomment(request)
 
 
 @api_view(['GET'])
-def get_comment(request, pk):
-    comment = Comment.objects.get(id=pk)
-    serializer = CommentSerializer(comment, many=False)
-    return Response(serializer.data)
+def get_subcomments_view(request):
+    return get_subcomments(request)
+
+
+# REACTION
+
+@api_view(['GET'])
+def get_reaction_view(request):
+    return get_reaction(request)
 
 
 @api_view(['GET'])
-def get_subcomments(request):
-    subcomments = Subcomment.objects.all()
-    serializer = SubcommentSerializer(subcomments, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def get_subcomment(request, pk):
-    subcomment = Subcomment.objects.get(id=pk)
-    serializer = SubcommentSerializer(subcomment, many=False)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def get_reactions(request):
-    reactions = Reaction.objects.all()
-    serializer = ReactionSerializers(reactions, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def get_reaction(request, pk):
-    reaction = Reaction.objects.get(id=pk)
-    serializer = ReactionSerializers(reaction, many=False)
-    return Response(serializer.data)
+def get_reactions_view(request):
+    return get_reactions(request)
