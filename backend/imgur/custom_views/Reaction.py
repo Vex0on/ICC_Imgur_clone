ab04947b419123f7bce0a74e3f490a7f8f18a7a0
MadPapa <happyekkov@gmail.com>
2023-03-19 01:36:31 +0100

    separate views of models

---
+++ backend/imgur/custom_views/Reaction.py
@@ -0,0 +1,17 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from ..models import Reaction
+from ..serializers import ReactionSerializers
+
+@api_view(['GET'])
+def get_reactions(request):
+    reactions = Reaction.objects.all()
+    serializer = ReactionSerializers(reactions, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_reaction(request, pk):
+    reaction = Reaction.objects.get(id=pk)
+    serializer = ReactionSerializers(reaction, many=False)
+    return Response(serializer.data)
