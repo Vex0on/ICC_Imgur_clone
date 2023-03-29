4fb7a1ce28e6d61623017f936876b7c5f55490f0
MadPapa <happyekkov@gmail.com>
2023-03-29 20:53:46 +0200

    hotfix for serialized object

--- backend/imgur/custom_views/Image.py
+++ backend/imgur/custom_views/Image.py
@@ -39,7 +39,7 @@ def create_image(request):
 def update_image(request, pk):
     try:
         image = Image.objects.get(id=pk)
-        serializer = Image(instance=image, data=request.data)
+        serializer = ImageSerializer(instance=image, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
