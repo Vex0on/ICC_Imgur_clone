e8f029226e67e266dc065fd23f9f0a9d460b41ac
Vex0on <Iplayagame101@gmail.com>
2023-04-04 20:58:44 +0200

    Add tests and fix serializers - Add tests for new User - Remove duplicated code in serializers - Fix code formatting in serializers

--- backend/imgur/tests.py
+++ backend/imgur/tests.py
@@ -9,56 +9,45 @@ from .models import ImgurUser, Image
 
 
 # Modele
-class UserModelTest(TestCase):
+class ImgurUserModelTest(TestCase):
     def setUp(self):
-        self.UserModel = get_user_model()
+        self.user_data = {
+            "email": "test@example.com",
+            "username": "test@example.com",
+            "password": "password123",
+            "phone_number": "123456789"
+        }
 
     def test_create_user(self):
-        password = "password123"
-        user = self.UserModel.objects.create_user(
-            username="testuser@gmail.com", password=password
+        password = 'password123'
+        user = ImgurUser.objects.create_user(
+            email='test@example.com',
+            username='test@example.com',
+            phone_number='123456789',
+            password=password
         )
-        # asserty sprawdzaja czy np. user.username zgadza sie z "testuser@gmail.com"
-        self.assertEqual(user.username, "testuser@gmail.com")
-        self.assertTrue(check_password(password, user.password))
+        self.assertEqual(user.email, 'test@example.com')
+        self.assertEqual(user.username, 'test@example.com')
+        self.assertEqual(user.phone_number, '123456789')
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
+        self.assertTrue(check_password(password, user.password))
 
     def test_create_superuser(self):
         password = "password123"
-        superuser = self.UserModel.objects.create_superuser(
-            username="testsuperuser@gmail.com", password=password
+        superuser = ImgurUser.objects.create_superuser(
+            email='testsuperuser@gmail.com',
+            username='testsuperuser@gmail.com',
+            phone_number='123456789',
+            password=password
         )
-        self.assertEqual(superuser.username, "testsuperuser@gmail.com")
-        # check_password sprawdza czy haslo zostało zapisane jako zahaszowane
-        self.assertTrue(check_password(password, superuser.password))
+        self.assertEqual(superuser.email, 'testsuperuser@gmail.com')
+        self.assertEqual(superuser.username, 'testsuperuser@gmail.com')
+        self.assertEqual(superuser.phone_number, '123456789')
         self.assertTrue(superuser.is_active)
-        self.assertTrue(superuser.is_staff)
         self.assertTrue(superuser.is_superuser)
-
-
-class ImgurUserModelTest(TestCase):
-    def setUp(self):
-        self.UserModel = get_user_model()
-
-    def test_create_imguruser(self):
-        user = self.UserModel.objects.create_user(
-            username="testuser@gmail.com", password="password123"
-        )
-        self.assertTrue(user.is_active)
-        self.assertFalse(user.is_staff)
-        self.assertFalse(user.is_superuser)
-
-        imguruser = ImgurUser.objects.create(
-            id=user.id,
-            phone_number="123456789",
-            username=user.username,
-            email=user.email,
-            password=user.password,
-            last_login=user.last_login,
-        )
-        self.assertEqual(imguruser.phone_number, "123456789")
+        self.assertTrue(check_password(password, superuser.password))
 
 
 class ImageModelTest(TestCase):
@@ -88,14 +77,14 @@ class ImgurUserTestCase(APITestCase):
     def setUp(self):
         self.client = APIClient()
         self.user = ImgurUser.objects.create(
-            username="testlogin@gmail.com",
+            email="testlogin@gmail.com",
             password=make_password("testpassword")
         )
 
     def test_register_user(self):
         url = reverse("register_user")  # funkcja reverse do uzyskania URL'a
         data = {
-            "username": "testuser1@gmail.com",
+            "email": "testuser1@gmail.com",
             "password": "password123",
         }
         response = self.client.post(url, data, format="json")
@@ -104,7 +93,7 @@ class ImgurUserTestCase(APITestCase):
     def test_register_user_with_existing_username(self):
         url = reverse("register_user")
         data = {
-            "username": "testuser1@gmail.com",
+            "email": "testuser1@gmail.com",
             "password": "password123",
         }
         response = self.client.post(url, data, format="json")
@@ -114,19 +103,19 @@ class ImgurUserTestCase(APITestCase):
 
     def test_register_user_with_missing_fields(self):
         url = reverse("register_user")
-        data = {"username": "testuser"}
+        data = {"email": "testuser"}
         response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_422_UNPROCESSABLE_ENTITY)
 
     def test_login_user(self):
         url = reverse("login_user")
         data = {
-            "username": "testlogin@gmail.com",
+            "email": "testlogin@gmail.com",
             "password": "testpassword",
         }
         response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        user = ImgurUser.objects.get(username="testlogin@gmail.com")
+        user = ImgurUser.objects.get(email="testlogin@gmail.com")
         self.assertTrue(check_password(data["password"], user.password))
 
     def test_get_imgur_users(self):
