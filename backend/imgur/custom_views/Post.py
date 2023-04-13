8b4fa8c7fb08d0a0d24ebb26da49d56171b6d8fa
sipmartyna <sipmartyna09@gmail.com>
2023-04-13 11:24:57 +0200

    Post CRUD created, image CRUD fixed

--- backend/imgur/custom_views/Post.py
+++ backend/imgur/custom_views/Post.py
@@ -17,3 +17,44 @@ def get_posts(request):
     posts = Post.objects.all()
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
+
+
+@api_view(["POST"])
+def create_post(request):
+    serializer = PostSerializer(data=request.data)
+    if serializer.is_valid():
+        post = serializer.save()
+        return Response(post, status=status.HTTP_201_CREATED)
+    return Response({"message": "HTTP_422_UNPROCESSABLE_ENTITY"}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+
+@api_view(["PUT"])
+def update_post(request, pk):
+    try:
+        post = Post.objects.get(id=pk)
+        serializer = PostSerializer(instance=post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(
+                {"message": "HTTP_400_BAD_REQUEST"}, status=status.HTTP_400_BAD_REQUEST
+            )
+    except Post.DoesNotExist:
+        return Response(
+            {"message": "HTTP_404_NOT_FOUND"}, status=status.HTTP_404_NOT_FOUND
+        )
+
+
+@api_view(["DELETE"])
+def delete_post(request, pk):
+    try:
+        post = Post.objects.get(id=pk)
+        post.delete()
+        return Response(
+            {"message": "HTTP_204_NO_CONTENT"}, status=status.HTTP_204_NO_CONTENT
+        )
+    except Post.DoesNotExist:
+        return Response(
+            {"message": "HTTP_404_NOT_FOUND"}, status=status.HTTP_404_NOT_FOUND
+        )
