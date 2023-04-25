2e11f31cc3450e57a581f6f21f6848e98bbd1103
kacperwadecki <44477935+kacperwadecki@users.noreply.github.com>
2023-04-25 20:36:40 +0200

    Add card

--- frontend/src/pages/PostPage/PostPage.tsx
+++ frontend/src/pages/PostPage/PostPage.tsx
@@ -3,19 +3,20 @@ import styles from './PostPage.module.scss'
 
 import { Nav } from '../../components/Home/Nav/Nav'
 import { Interactions } from '../../components/Post/Interactions/Interactions'
+import { Card } from '../../components/Post/Card/Card'
 
 export const PostPage = () => {
     return(
         <>
             <Nav />
 
-            <main>
+            <main className={styles.main}>
                 <div>
                     <Interactions />
                 </div>
 
                 <div>
-
+                    <Card />
                 </div>
 
                 <div>
