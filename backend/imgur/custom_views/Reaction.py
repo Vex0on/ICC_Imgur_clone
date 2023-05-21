cd97429c64471b33777e5ad080518cfe77e885ac
Szymon <szwielgosz259@gmail.com>
2023-05-21 15:21:12 +0200

    Add views and urls for checking reactions

--- backend/imgur/custom_views/Reaction.py
+++ backend/imgur/custom_views/Reaction.py
@@ -1,4 +1,5 @@
 from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -22,22 +23,39 @@ class ReactionList(APIView):
             )
 
     def post(self, request):
-        serializer = ReactionSerializers(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
+        record_id = request.data.get('record_id')
+        individual_id = request.data.get('individual_id')
+        imgur_user_id = request.data.get('imgur_user')
+
+        try:
+            existing_reaction = Reaction.objects.get(record_id=record_id, individual_id=individual_id,
+                                                     imgur_user=imgur_user_id)
+            serializer = ReactionSerializers(existing_reaction)
             return Response(
-                {"message": "HTTP_201_CREATED"},
-                status=status.HTTP_201_CREATED,
+                {"message": "HTTP_409_CONFLICT", "existing_reaction": serializer.data},
+                status=status.HTTP_409_CONFLICT,
             )
-        else:
-            return Response(
-                {"message": "HTTP_400_BAD_REQUEST"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
+        except Reaction.DoesNotExist:
+            serializer = ReactionSerializers(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    {"message": "HTTP_201_CREATED"},
+                    status=status.HTTP_201_CREATED,
+                )
+            else:
+                return Response(
+                    {"message": "HTTP_400_BAD_REQUEST"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
 
 
 class ReactionDetail(APIView):
-    def get(self, request, record_id, individual_id, imgur_user_id):
+    def get(self, request):
+        record_id = request.data.get('record_id')
+        individual_id = request.data.get('individual_id')
+        imgur_user_id = request.data.get('imgur_user')
+
         try:
             reaction = Reaction.objects.get(record_id=record_id, individual_id=individual_id, imgur_user_id=imgur_user_id)
             serializer = ReactionSerializers(reaction, many=False)
@@ -84,4 +102,37 @@ class ReactionDetail(APIView):
             return Response(
                 {"message": "HTTP_404_NOT_FOUND"},
                 status=status.HTTP_404_NOT_FOUND,
+            )
+
+
+class CountReactions(APIView):
+    def get(self, request):
+        record_id = request.data.get('record_id')
+        individual_id = request.data.get('individual_id')
+
+        try:
+            like_data = Reaction.objects.filter(record_id=record_id, individual_id=individual_id, reaction=True).count()
+            dislike_data = Reaction.objects.filter(record_id=record_id, individual_id=individual_id, reaction=False).count()
+            count = like_data - dislike_data
+            return Response({"count": count}, status=status.HTTP_200_OK)
+        except Reaction.DoesNotExist:
+            return Response(
+                {"message": "HTTP_404_NOT_FOUND"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+
+class UserReaction(APIView):
+    def get(self, request):
+        record_id = request.data.get('record_id')
+        individual_id = request.data.get('individual_id')
+        imgur_user_id = request.data.get('imgur_user')
+
+        try:
+            reaction = Reaction.objects.get(record_id=record_id, individual_id=individual_id, imgur_user=imgur_user_id)
+            return Response({"reaction": reaction.reaction})
+        except Reaction.DoesNotExist:
+            return Response(
+                {"message": "HTTP_404_NOT_FOUND"},
+                status=status.HTTP_404_NOT_FOUND
             )
