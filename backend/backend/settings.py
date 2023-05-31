bbfa9138ef708ae36f89307b96135977ddcca0ef
Vex0on <Iplayagame101@gmail.com>
2023-06-01 01:18:03 +0200

    Remove Docker

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -94,11 +94,11 @@ WSGI_APPLICATION = "backend.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": "db",
-        "PORT": 5432,
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": "B6JzfVUfL7govHDmJbiB",
+        "HOST": "containers-us-west-62.railway.app",
+        "PORT": 7644,
     }
 }
 
